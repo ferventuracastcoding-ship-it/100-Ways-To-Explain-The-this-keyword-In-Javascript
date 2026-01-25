1cf1b34243ceae54f0968d099b9dd66ff8b12ffd
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-01-24 20:48:56 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Scene;
+import javafx.scene.control.label;
+import javafx.stage.Stage;
+
+Public class HelloWorldG
+
+public class HelloWorldGUI extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello World");
+        
+   Scene scene = new Scene(label, 300, 100)
+        stage.setScene(scene);
+        stage.setTitle("Hello World GUI");
+        stage.show();
+    }
+
+    public static void main(Strkng[] args) {
+        launch();
+    }
+}
