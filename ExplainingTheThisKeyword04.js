a0bcd3d80f91e8f69a0809781320af573948a97a
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-07-06 07:16:55 -0700

    Add data variables in ExplainingTheThisKeyword04.js
    Added variables for planet, year, status, visitors, and sos.

--- ExplainingTheThisKeyword04.js
+++ ExplainingTheThisKeyword04.js
@@ -1,5 +1,10 @@
 const data {
   key: value() {
-    // add code
+    // add data
+    const planet = "Trappist 1e";
+    const year = 2281;
+    const status = "Not found 404";
+    const visitors = 404;
+    const sos = ${planet} ${status};
   }
 }
