2a022872721d51427a342905b3af6dd07d1c415e
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-07-17 17:09:05 -0700

    Add sorting examples for arrays
    Sort arrays in ascending and descending order.

---
+++ ExplainingTheThisKeyword.js
@@ -0,0 +1,12 @@
+const numbers = [8, 3, 15, 1, 9];
+
+numbers.sort((a, b) => a - b);
+
+console.log(numbers);
+// [1, 3, 8, 9, 15]
+
+const nums = [8, 3, 15, 1, 9];
+
+nums.sort((a, b) => b - a);
+
+console.log(nums);
