577a58ead11bfadcbc26efcad8bc0f71270a857f
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-07-17 17:19:55 -0700

    Log 'this' and define app function
    Added console log for 'this' and defined 'app' function.

--- ExplainingTheThisKeyword.js
+++ ExplainingTheThisKeyword.js
@@ -3,6 +3,11 @@ const numbers = [8, 3, 15, 1, 9];
 numbers.sort((a, b) => a - b);
 
 console.log(numbers);
+console.log(this);
+function app(param1, param2) {
+  name = this.name;
+  calling = this.calling;
+}
 // [1, 3, 8, 9, 15]
 
 const nums = [8, 3, 15, 1, 9];
