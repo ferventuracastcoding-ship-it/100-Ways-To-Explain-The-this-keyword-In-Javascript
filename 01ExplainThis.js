b4740dfc7f11f56dd998710d3a2d63d39ab2cecd
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-01 15:58:53 -0700

    Add Explain class with Cable function and placeholders

---
+++ 01ExplainThis.js
@@ -0,0 +1,15 @@
+class Explain {
+  const win = console.log(this);
+
+  constructor(){}
+  function Cable(a, b){
+    const score = 1200;
+    const board = 5000;
+    const run = 25000;
+
+    return 0;
+  }
+
+  getters(){}
+  setters(){}
+}
