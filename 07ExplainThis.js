75e864aa849ba5475335e3060efdae30fcd0c28a
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-10 16:47:25 -0700

    Add initial constants for data structures

---
+++ 07ExplainThis.js
@@ -0,0 +1,11 @@
+const LonkedList = 0;
+const this.data = 1;
+
+const BinarySearch = 0;
+const this.search = 1;
+
+const SortArray = 0;
+const this.sort = 1;
+
+const MapIndex = 0;
+const this.index = 1;
