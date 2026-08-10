a38cfd4302b6d3cd6a43c8686880c34097f8b256
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-09 22:05:46 -0700

    Initialize 06ExplainThis.js with variables and comments
    Add initial content to 06ExplainThis.js with comments and variable declarations.

---
+++ 06ExplainThis.js
@@ -0,0 +1,14 @@
+const LinkedList = 0;
+
+// nonsense
+const data = this.data;
+const data = this.data;
+const data = this.data;
+const data = this.data;
+
+
+const value = null;
+const value = null;
+const value = null;
+
+// Explain the this keyword
