49bb6569cee9d053bf1c7f1e6b15402e217137da
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-11 20:11:42 -0700

    Implement head property and insertion method in LinkedList
    Added head property and insertAtBeginning method to LinkedList class.

--- 09ExplainThis.js
+++ 09ExplainThis.js
@@ -6,6 +6,12 @@ class Node {
   class LinkedList (data) {
     constructor(data) {
       // explain this
+      this.head = null;
+    }
+    insertAtBeginning(data){
+      const node = new Node(data);
+      node.next = this.head;
+      this.head = node;
     }
   }
 }
