75f24fe4f3ef340a66ba27f1c2a76e62a7c24bd6
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-14 22:34:22 -0700

    Add initial structure for app, Node, and LinkedList classes

---
+++ 10ExplainThis.js
@@ -0,0 +1,25 @@
+function app {
+  const data = this.data;
+  const value = this.value;
+  const key = this.key;
+  return this;
+}
+
+class Node {
+  constructor(){
+    this.data = data;
+    this.value = null;
+  }
+}
+
+class LinkedList {
+  constructor(data) {
+    this.value = null;
+  }
+}
+
+isAtBeginning() {
+  while() {
+    if() {}
+  }
+}
