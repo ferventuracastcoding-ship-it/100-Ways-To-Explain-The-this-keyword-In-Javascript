2050503327ee86a418aa72e2e9263c3ba607a559
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-15 18:16:25 -0700

    Add initial implementation of mars function

---
+++ 11ExplainThis.js
@@ -0,0 +1,10 @@
+// Explaining the this keyword
+function mars(earth) {
+  constructor() {
+    this.data = data;
+    this.value = value 
+  }
+}
+
+const rover = map(rockSamples => rockSamples * 2);
+
