0156d12ceb914652cf8777861e15154d90e5da85
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-18 01:00:59 -0700

    Add nested loops with console log statements

---
+++ 20ExplainThis.js
@@ -0,0 +1,20 @@
+for(let i = 0; i < 10; i++) {
+    console.log("A.I. root");
+  for(let i = 0; j < 10; j++) {
+    console.log("Warp Drive to Mars!")
+    array[this];
+  }
+}
+
+for(let a = 0; a <= 10; a++){
+    console.log("");
+  for(let a = 0; a <= 10; a++){
+      console.log("");
+    for(let a = 0; a <= 10; a++){
+        console.log("*");
+      for(let b = 0; b <= 10; b++) {
+          console.log("this");
+      }
+    }
+  }
+}
