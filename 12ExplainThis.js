ed0aeb89d3219d08a20f4ce52d64bc25dd6132ed
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-23 14:31:17 -0700

    Add spaceship object with showInfo method

---
+++ 12ExplainThis.js
@@ -0,0 +1,13 @@
+spaceship.showInfo();
+const spaceship = {
+  name: "Starship",
+  speed: 25000,
+  
+  showInfo() {
+    console.log(this.name);
+    console.log(this.speed);
+  }
+}
+
+// Starship
+// 25000
