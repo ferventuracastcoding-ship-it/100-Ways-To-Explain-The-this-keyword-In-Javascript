b974f2e00089431e5b12bce53e19176e7f5b9f3f
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-09-07 17:28:18 -0700

    Add initial Linked List and Node structure

---
+++ 23ExplainThis.js
@@ -0,0 +1,17 @@
+// Explaining the this keyword in a Linked List
+const Nide {
+  constructor(data) {
+    this.data = data;
+    this.value = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.next = null;
+  }
+}
+
+delete (node) {
+  while() {}
+}
